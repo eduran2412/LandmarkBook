b1b58864dec90f5d8efc331caa566a129394a615
TOSHIBA <57902145+eduran2412@users.noreply.github.com>
2021-09-16 17:12:05 +0300

    modeling was done

---
+++ app/src/main/java/com/erenduran/landmarkbook/Landmark.java
@@ -0,0 +1,18 @@
+package com.erenduran.landmarkbook;
+
+public class Landmark {
+
+    String name;
+    String country;
+    int image;
+
+    // constructor
+
+    public Landmark(String name, String country, int image){
+        this.name = name;
+        this.country = country;
+        this.image = image;
+
+
+    }
+}
